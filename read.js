a9dfa5b0acecfb19c529f3613713fa86731e9b5c
Mehak <mehakzehra5@gmail.com>
2023-10-11 12:45:43 -0700

    read $ update

--- read.js
+++ read.js
@@ -5,5 +5,7 @@ db.inventory.find({tags:{$in:["silver", "silk" ]}})
 //ADD operator
 db.inventory.find( { status: "A", qty: { $lt: 30 } } )
 
-//OR operator
+//OR operator-- needs any codition to be true for pull information
 db.inventory.find( { $or: [ { status: "A" }, { qty: { $lt: 30 } } ] } )
+// to pull one whole document not any array
+db.inventory.findOne( { $or: [ { status: "A" }, { qty: { $lt: 30 } } ] } )
