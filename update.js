c84592939f58f9e156b937496a65ac22c1648a54
Mehak <mehakzehra5@gmail.com>
2023-10-11 12:57:15 -0700

    delete

--- update.js
+++ update.js
@@ -5,3 +5,23 @@ db.inventory.updateOne(
       $currentDate: { lastModified: true }
     }
  )
+ // firdt find all docuements by using find code  then select any one then update it
+ // if ant document has last modified then ot update it otherwise it simply add that 
+
+
+ // update many 
+ db.inventory.updateMany(
+    { "qty": { $lt: 50 } },
+    {
+      $set: { "size.uom": "in", status: "P" },
+      $currentDate: { lastModified: true }
+    }
+ )
+
+ // find one and replace 
+ // it find that document and replace it all with new update
+
+ db.inventory.replaceOne(
+    { item: "paper" },
+    { item: "paper", instock: [ { warehouse: "A", qty: 60 }, { warehouse: "B", qty: 40 } ] }
+ )
